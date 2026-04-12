b9da61ad583326261b00836b2361a0d0d5d3306c
Emanuel Taquichiri <thecobra1783@gmail.com>
2026-04-12 18:30:25 -0400

    Consultorio controller and route

---
+++ backend/src/routes/consultorioRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const consultorioController = require('../controllers/consultorioController');
+
+// Ruta: /vitalia/consultorio
+
+module.exports = router;
