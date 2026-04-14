1d890f8e0f24871772b57246611fff235c72df82
Emanuel Taquichiri <thecobra1783@gmail.com>
2026-04-13 22:10:11 -0400

    Especialidad config in app.js, and endpoint for get all especialidad and get by id

--- backend/src/routes/especialidadRoutes.js
+++ backend/src/routes/especialidadRoutes.js
@@ -2,4 +2,8 @@ const express = require('express');
 const router = express.Router();
 const especialidadController = require('../controllers/especialidadController');
 
+// Ruta: /vitalia/especialidad
+router.get('/', especialidadController.obtenerEspecialidades);
+router.get('/:id', especialidadController.obtenerEspecialidadPorId);
+
 module.exports = router;
