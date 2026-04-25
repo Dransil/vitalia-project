c62d4d4c6c489da0b5648037975836157ead07d3
Emanuel Taquichiri <thecobra1783@gmail.com>
2026-04-24 22:06:40 -0400

    Fix routes for historialDental with files

--- backend/src/routes/historialDentalRoutes.js
+++ backend/src/routes/historialDentalRoutes.js
@@ -3,10 +3,15 @@ const router = express.Router();
 const historialdentalController = require('../controllers/historialDentalController');
 const upload = require('../middlewares/upload');
 
+const fotosUpload = upload.fields([
+    { name: 'foto_antes',   maxCount: 1 },
+    { name: 'foto_despues', maxCount: 1 }
+]);
+
 // Ruta: /vitalia/historialdental
 router.get('/', historialdentalController.obtenerHistorialesDentales);
 router.get('/:id', historialdentalController.obtenerHistorialDentalPorId);
-router.post('/', historialdentalController.crearHistorialDental);
-router.put('/:id', historialdentalController.actualizarHistorialDental);
+router.post('/', fotosUpload ,historialdentalController.crearHistorialDental);
+router.put('/:id', fotosUpload, historialdentalController.actualizarHistorialDental);
 
 module.exports = router;
